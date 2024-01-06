3299de08e53375887c2a451005c6df301124c6da
sandesh gaikwad <sandesh.gaikwad1911@gmail.com>
2024-01-06 22:29:24 +0530

    explore page

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,7 +1,139 @@
 import './explore.scss'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Select from "react-select";
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper';
+import MovieCard from '../../components/movieCard/MovieCard';
+import Spinner from '../../components/spinner/Spinner';
+import { fetchDataFromApi } from '../../utils/api';
+
+let filters = {};
+
+const sortbyData = [
+    { value: "popularity.desc", label: "Popularity Descending" },
+    { value: "popularity.asc", label: "Popularity Ascending" },
+    { value: "vote_average.desc", label: "Rating Descending" },
+    { value: "vote_average.asc", label: "Rating Ascending" },
+    {
+        value: "primary_release_date.desc",
+        label: "Release Date Descending",
+    },
+    { value: "primary_release_date.asc", label: "Release Date Ascending" },
+    { value: "original_title.asc", label: "Title (A-Z)" },
+];
+
+
 const Explore = () => {
+
+    const [data, setData] = useState(null);
+    const [pageNum, setPageNum] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [sortby, setSortby] = useState(null);
+    const { mediaType } = useParams();
+
+    const fetchInitialData = ()=>{
+      setLoading(true);
+      fetchDataFromApi(`/discover/${mediaType}`, filters)
+      .then((res)=>{
+        setData(res);
+        setPageNum((prev)=>prev + 1)
+        setLoading(false);
+      })
+    }
+
+    const fetchNextPageData = ()=>{
+      fetchDataFromApi(`/discover/${mediaType}?page=${pageNum}`,filters)
+        .then((res)=>{
+          if(data?.results){
+            setData({...data, results:[...data.results, ...res.results]})
+          }
+          else{
+            setData(res);
+          }
+          setPageNum((prev)=>prev + 1)
+        })
+    }
+
+    useEffect(()=>{
+      filters={}
+      setData(null)
+      setPageNum(1)
+      setSortby(null)
+      fetchInitialData();
+    },[mediaType])
+
+    const onChange = (selectedItems, action)=>{
+
+        if(action.name === "sortby"){
+          setSortby(selectedItems)
+          if(action.action !== "clear"){
+            filters.sort_by = selectedItems.value;
+          }else{
+            delete filters.sort_by;
+          }
+        }
+
+        setPageNum(1)
+        fetchInitialData();
+
+    }
+
   return (
-    <div>Explore</div>
+    <div className="explorePage">
+      <ContentWrapper>
+        <div className="pageHeader">
+          <div className="pageTitle">
+            {
+              mediaType == "tv" ? "Explore TV Shows" : "Explore Movies"
+            }
+          </div>
+          <div className="filters">
+              <Select
+                    name="sortby"
+                    value={sortby}
+                    options={sortbyData}
+                    onChange={onChange}
+                    isClearable={true}
+                    placeholder="Sort by"
+                    className="react-select-container sortbyDD"
+                    classNamePrefix="react-select"
+              />
+          </div>
+        </div>
+        {
+          loading && (<Spinner initial={true} />)
+        }
+        {
+          !loading && (
+            <>
+              {
+                data?.results?.length > 0 ? (
+                  <InfiniteScroll
+                    className="content"
+                    dataLength={data?.results?.length || []}
+                    next={fetchNextPageData}
+                    hasMore={pageNum <= data?.total_pages}
+                    loader={<Spinner />}
+                  >
+                      {
+                        data?.results?.map((item, i)=>{
+                          if (item.media_type === "person") return;
+                          return(
+                            <MovieCard key={i} data={item} mediaType={mediaType}/>
+                          )
+                        })
+                      }
+                  </InfiniteScroll>
+                ) : (
+                  <span className="resultNotFound">Sorry, no result found !</span>
+                )
+              }
+            </>
+          )
+        }
+      </ContentWrapper>
+    </div>
   )
 }
 
